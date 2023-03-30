95f9f3ed4d111e2995fbd5395bd9f78e1b933c9e
Lemson Mureya <lmureya@conncoll.edu>
2023-03-30 19:14:54 -0400

    added gitign

--- smeProject/settings.py
+++ smeProject/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-558&4fl=a1oz8qblutz)j907r*rp36pj$y3!7=3jx0gf9sr)f7'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
