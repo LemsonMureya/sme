274d170ceb1006849ec1ba2e3de06e5ba42a292e
Lemson Mureya <lmureya@conncoll.edu>
2023-05-25 20:37:33 -0400

    Updated thumbnail template tags

--- smeApp/templatetags/thumbnails.py
+++ smeApp/templatetags/thumbnails.py
@@ -2,22 +2,62 @@ from django import template
 from django.conf import settings
 from PIL import Image
 import os
+import hashlib
+import boto3
+from uuid import uuid4
+import io
+import botocore.exceptions
+import requests
 
 register = template.Library()
 
 @register.filter
 def get_thumbnail_url(image_url):
-    file_path = os.path.join(settings.MEDIA_ROOT, image_url.replace(settings.MEDIA_URL, ""))
+    # Hash the image_url to create a unique thumbnail filename
+    hash_object = hashlib.md5(image_url.encode())
+    hex_dig = hash_object.hexdigest()
+    thumbnail_key = 'thumbnails/{}.{}'.format(hex_dig, 'png')
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+        region_name=os.environ.get('AWS_S3_REGION_NAME')
+    )
+
+    bucket_name = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+    # Check if the thumbnail already exists in S3
+    try:
+        s3.head_object(Bucket=bucket_name, Key=thumbnail_key)
+        return 'https://{}.s3.amazonaws.com/{}'.format(bucket_name, thumbnail_key)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] != "404":
+            # Something else has gone wrong.
+            raise
+
+    # Download image from S3
+    response = requests.get(image_url)
+    image = Image.open(io.BytesIO(response.content))
 
     # Create thumbnail
-    image = Image.open(file_path)
     thumbnail_size = (200, 200)
     image.thumbnail(thumbnail_size)
 
-    # Save thumbnail
-    thumbnail_filename = os.path.splitext(file_path)[0] + "_thumbnail" + os.path.splitext(file_path)[1]
-    image.save(thumbnail_filename)
+    # Save thumbnail to a temporary file
+    temp_file = io.BytesIO()
+    image.save(temp_file, format='PNG')
+    temp_file.seek(0)
 
-    # Return the thumbnail URL
-    thumbnail_url = os.path.join(settings.MEDIA_URL, os.path.relpath(thumbnail_filename, settings.MEDIA_ROOT)).replace("\\", "/")
-    return thumbnail_url
+    # Upload the thumbnail to S3 and return the new URL
+    s3.upload_fileobj(
+        temp_file,
+        bucket_name,
+        thumbnail_key,
+        ExtraArgs={
+            'ACL': 'public-read',
+            'ContentType': 'image/png'
+        }
+    )
+
+    return 'https://{}.s3.amazonaws.com/{}'.format(bucket_name, thumbnail_key)
